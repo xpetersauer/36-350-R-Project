a5b8586564d3e628324fc2845320d50f7cbbda76
PSauer <pjsauer@gmail.com>
2021-08-05 13:33:55 -0700

    This is my message to Aliens

---
+++ hello_aliens.R
@@ -0,0 +1,62 @@
+cat("Dear Aliens, \n
+    Hello! How are you? Hope your Summer is going well :) \n
+    On Earth, we have different seasons (cycles of different environments).\n
+    We refer to Summer as the 'hot environment' on the upper hemisphere of Earth \n
+    (or the part closer to the north pole). Now that I think about it though, Earth's \n
+    axis might be completely screwed so that 2021's North is actually screwed. \n
+    Anywho, I hope you are friendly to humans. Someone of us are not cool, but some of \n
+    us are! I hope your civilization isn't trying to conquer us. War is bad! \n
+    As a welcoming offer, I will type out one of our most valuable pieces of music, \n
+    the American National Anthem. On behalf of Earth, we welcome you with open arms! \n
+    \n
+    Astro-naut \n
+    What you know about rollin' down in the deep? \n
+    When your brain goes numb, you can call that mental freeze \n
+    When these people talk too much, put that poopie in slow motion, yeah \n
+    I feel like an astronaut in the ocean, ayy \n
+    What you know about rollin' down in the deep? \n
+    When your brain goes numb, you can call that mental freeze \n
+    When these people talk too much, put that poopie in slow motion, yeah \n
+    I feel like an astronaut in the ocean \n
+    She say that I'm cool (factual) \n
+    I'm like 'yeah, that's true' (that's true) \n
+    I believe in G-O-D (ayy) \n
+    Don't believe in T-H-O-T \n
+    She keep playing me dumb (play me) \n
+    I'ma play her for fun (uh-huh) \n
+    Y'all don't really know my mental \n
+    Lemme give you the picture like stencil \n
+    Falling out, in a drought \n
+    No flow, rain wasn't pouring down (pouring down) \n
+    See, that pain was all around \n
+    See, my mode was kinda lounged \n
+    Didn't know which-which way to turn \n
+    Flow was cool but I still felt burnt \n
+    Energy up, you can feel my surge \n
+    I'ma relieve everything like this purge (ayy) \n
+    Let's just get this straight for a second, I'ma work \n
+    Even if I don't get paid for progression, I'ma get it (get it) \n
+    Everything that I do is electric \n
+    I'ma keep it in a motion, keep it moving like kinetic, ayy (yeah, yeah, yeah, yeah) \n
+    Put this poopie in a frame, better know I don't blame \n
+    Everything that I say, man I seen you deflate \n
+    Let me elevate, this ain't a prank \n
+    Have you walkin' on a plank, la-la-la-la-la, like \n
+    Both hands together, God, let me pray (now let me pray) \n
+    Uh, I've been going right, right around, call that relay (Masked Wolf) \n
+    Pass the baton, back and I'm on \n
+    Swimming in the pool, Kendrick Lamar, uh \n
+    Want a piece of this, a piece of mine, my peace a sign \n
+    Can you please read between the lines? \n
+    My rhyme's inclined to break your spine \n
+    They say that I'm so fine \n
+    You could never match my grind \n
+    Please do not, not waste my time \n
+    What you know about rollin' down in the deep? \n
+    When your brain goes numb, you can call that mental freeze \n
+    When these people talk too much, put that poopie in slow motion, yeah \n
+    I feel like an astronaut in the ocean, ayy \n
+    What you know about rollin' down in the deep? \n
+    When your brain goes numb, you can call that mental freeze \n
+    When these people talk too much, put that poopie in slow motion, yeah \n
+    I feel like an astronaut in the ocean")
